b4232a1227687d7f0049aa289792f0ef4dcc5f4b
ch-saikumar-720 <158131442+ch-saikumar-720@users.noreply.github.com>
2025-09-14 18:07:37 +0530

    Update backend.tf

--- iac_tf/backend.tf
+++ iac_tf/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket  = "my-terraform-state-bucket" # Must already exist
+    bucket  = "my-terraform-state-us-east1-unique"
     key     = "project_2/kubernetes_project/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
 }
-
