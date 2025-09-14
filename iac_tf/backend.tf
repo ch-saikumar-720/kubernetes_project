80f7978ddae0979aeac397f911e7ae5a7f5c4423
ch-saikumar-720 <chsaikumar720@gmail.com>
2025-09-14 16:41:20 +0530

    chnaged

--- iac_tf/backend.tf
+++ iac_tf/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"                      # Replace with your S3 bucket
-    key            = "project_2/kubernetes_project/terraform.tfstate" # Path inside the bucket
-    region         = "us-east-1"                                      # AWS region
-    dynamodb_table = "terraform-locks"                                # Optional: for state locking
-    encrypt        = true
+    bucket  = "my-terraform-state-bucket" # Must already exist
+    key     = "project_2/kubernetes_project/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
 
