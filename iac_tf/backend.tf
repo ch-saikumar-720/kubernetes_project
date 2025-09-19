b7a07b70830441af99c18d431c6b3323af24103b
ch-saikumar-720 <158131442+ch-saikumar-720@users.noreply.github.com>
2025-09-19 11:48:05 +0530

    deleted the terraform files

--- iac_tf/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "my-terraform-state-us-east1-unique"
-    key     = "project_2/kubernetes_project/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
