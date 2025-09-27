4612b418259d40dcbe9bd7387da26ab63d8be101
ch-saikumar-720 <158131442+ch-saikumar-720@users.noreply.github.com>
2025-09-27 15:04:03 +0530

    Update app.py updated rds host

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ CORS(app)
 bcrypt = Bcrypt(app)
 
 # Database Configuration (Replace with your AWS RDS credentials)
-DB_HOST = "172.17.0.2"
+DB_HOST = "public-mysql-db.co9uaqwkoizy.us-east-1.rds.amazonaws.com"
 DB_USER = "kartikuser"
 DB_PASSWORD = "password"
 DB_NAME = "kartikdatabase"
@@ -63,3 +63,4 @@ def login():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
