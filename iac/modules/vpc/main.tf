6efa5d59613c210a391e6a0acc4579003e309df9
ch-saikumar-720 <chsaikumar720@gmail.com>
2025-09-27 11:48:39 +0530

    updated chnages

--- iac/modules/vpc/main.tf
+++ iac/modules/vpc/main.tf
@@ -31,3 +31,43 @@ resource "aws_subnet" "private" {
   }
 }
 
+# ------------------------
+# Internet Gateway
+# ------------------------
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+# ------------------------
+# Public Route Table
+# ------------------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+# ------------------------
+# Default route to Internet Gateway
+# ------------------------
+resource "aws_route" "default_public_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# ------------------------
+# Associate Public Route Table with Public Subnets
+# ------------------------
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
+
